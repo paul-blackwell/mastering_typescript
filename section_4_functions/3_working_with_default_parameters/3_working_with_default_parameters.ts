b379df5a3c1487ae937516564cae0e69d9488aa5
paul-blackwell <paul@paulblackwell.io>
2023-06-11 09:08:15 +0100

    Completed the following sections in section 4:
    + 3_working_with_default_parameters.ts

---
+++ section_4_functions/3_working_with_default_parameters/3_working_with_default_parameters.ts
@@ -0,0 +1,33 @@
+function square(num: number) {
+  return num * num;
+}
+
+function greet(person: string) {
+  return `Hi there, ${person}!`;
+}
+
+square(3); // Will work
+// square('asasa'); Won't work
+
+greet('Paul'); // Will work
+// greet(true) // Won't work
+
+// Multiple parameters
+
+// Standard function
+function doSomething(person: string, age: number, isFunny: boolean) {}
+
+// Arrow function
+// const doSomethingArrow = (person: string, age: number, isFunny: boolean) => {};
+
+doSomething('Paul', 20, true); // Will work
+// doSomething('Paul'); won't work
+// doSomething(20, 'Paul', true); // won't work
+
+
+// With default parameter
+function greetWithDefaultParameter(person: string = 'stranger') {
+  return `Hi there, ${person}!`;
+}
+
+greetWithDefaultParameter();
