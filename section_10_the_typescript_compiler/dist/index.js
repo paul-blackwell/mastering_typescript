086ce6ea004d1476269830866190ecfdac7ac90c
paul-blackwell <paul@paulblackwell.io>
2023-08-12 08:37:59 +0100

    Completed the following sections in section 10: + 1 Compiling to JavaScript + 2 Watch Mode + 3  Working With Multiple Files + 4 The Files Compiler Option + 5 Include & Exclude Options + 6 outdir option +

---
+++ section_10_the_typescript_compiler/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: 'Slikie',
+    eggsPerWeek: 4,
+    name: 'Norma',
+    age: 2,
+};
+const juniper = {
+    breed: 'Slikie',
+    eggsPerWeek: 5,
+    name: 'Juniper',
+    age: 1,
+};
