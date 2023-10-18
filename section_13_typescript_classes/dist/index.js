bbb12818134f91b1553330f39b2467f6775e9eb9
paul-blackwell <paul@paulblackwell.io>
2023-10-18 08:34:54 +0100

    Completed the following sections in section 13: + Annotating Classes In TypeScript + Class Fields In TypeScript + readonly Class Properties + The public Modifier + The private Modifier

--- section_13_typescript_classes/dist/index.js
+++ section_13_typescript_classes/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('test');
+class Player {
+    constructor(first, last) {
+        this.score = 0;
+        this.first = first;
+        this.last = last;
+    }
+    secretMethod() {
+        console.log('Secret method');
+    }
+}
+const elton = new Player('Elton', 'Steele');
+console.log(elton);
--- section_13_typescript_classes/src/index.ts
+++ section_13_typescript_classes/src/index.ts
@@ -1,1 +1,19 @@
-console.log('test');
+class Player {
+  // Once you set first and last you can't change them (readonly)
+  public readonly first: string;
+  public readonly last: string;
+
+  public score: number = 0;
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+
+  private secretMethod(): void {
+    console.log('Secret method');
+  }
+}
+
+const elton = new Player('Elton', 'Steele');
+console.log(elton)
