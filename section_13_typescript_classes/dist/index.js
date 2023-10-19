5a5f3fe09dae3fcc12d9f2780396af4ffb3f921a
paul-blackwell <paul@paulblackwell.io>
2023-10-19 08:27:14 +0100

    Completed the following sections in section 13: + Parameter Properties Shorthand + Getters and Setters + The Protected Modifier + Classes and Interfaces

--- section_13_typescript_classes/dist/index.js
+++ section_13_typescript_classes/dist/index.js
@@ -1,13 +1,87 @@
 "use strict";
+// class Player {
+//   // Once you set first and last you can't change them (readonly)
+//   public readonly first: string;
+//   public readonly last: string;
+//   public score: number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+//   private secretMethod(): void {
+//     console.log('Secret method');
+//   }
+// }
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+    // public - is accessible everywhere
+    // private -  only accessible in the class
+    // protected - protected is only accessible in class and child classes
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
+        this._score = _score;
     }
     secretMethod() {
         console.log('Secret method');
     }
+    get fullName() {
+        return `${this.first}, ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0)
+            throw new Error('Score can not be negative');
+        this._score = newScore;
+    }
 }
-const elton = new Player('Elton', 'Steele');
-console.log(elton);
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+    maxScore() {
+        this._score = 999999999;
+    }
+}
+const elton = new Player('Elton', 'Steele', 100);
+console.log(elton.fullName);
+elton.score = 99;
+class Bike {
+    constructor(color) {
+        this.color = color;
+    }
+}
+class Jacket {
+    constructor(brand, color) {
+        this.brand = brand;
+        this.color = color;
+    }
+    print() {
+        console.log(`${this.color} ${this.brand} jacket`);
+    }
+}
+const bike1 = new Bike('red');
+const jacket1 = new Jacket('Prada', 'black');
+/* -------------------------------------------------------------- */
+// Abstract Classes
+class Employee {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+    greet() {
+        console.log('Hello');
+    }
+}
+class FullTimeEmployee extends Employee {
+    getPay() {
+        return 13;
+    }
+}
+class PartTimeEmployee extends Employee {
+    getPay() {
+        return 6.5;
+    }
+}
--- section_13_typescript_classes/src/index.ts
+++ section_13_typescript_classes/src/index.ts
@@ -1,19 +1,102 @@
+// class Player {
+//   // Once you set first and last you can't change them (readonly)
+//   public readonly first: string;
+//   public readonly last: string;
+
+//   public score: number = 0;
+
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//   }
+
+//   private secretMethod(): void {
+//     console.log('Secret method');
+//   }
+// }
+
 class Player {
-  // Once you set first and last you can't change them (readonly)
-  public readonly first: string;
-  public readonly last: string;
+  // public - is accessible everywhere
+  // private -  only accessible in the class
+  // protected - protected is only accessible in class and child classes
 
-  public score: number = 0;
-
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
-  }
+  constructor(public first: string, public last: string, protected _score: number) {}
 
   private secretMethod(): void {
     console.log('Secret method');
   }
+
+  get fullName(): string {
+    return `${this.first}, ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) throw new Error('Score can not be negative');
+    this._score = newScore;
+  }
 }
 
-const elton = new Player('Elton', 'Steele');
-console.log(elton)
+class SuperPlayer extends Player {
+  public isAdmin: boolean = true;
+  maxScore() {
+    this._score = 999999999;
+  }
+}
+
+const elton = new Player('Elton', 'Steele', 100);
+console.log(elton.fullName);
+elton.score = 99;
+
+/* -------------------------------------------------------------- */
+
+interface Colorful {
+  color: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class Bike implements Colorful {
+  constructor(public color: string) {}
+}
+
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) {}
+
+  print(): void {
+    console.log(`${this.color} ${this.brand} jacket`);
+  }
+}
+
+const bike1 = new Bike('red');
+
+const jacket1 = new Jacket('Prada', 'black');
+
+/* -------------------------------------------------------------- */
+
+// Abstract Classes
+
+abstract class Employee {
+  constructor(public first: string, public last: string) {}
+  abstract getPay(): number;
+  greet() {
+    console.log('Hello');
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  getPay(): number {
+    return 13;
+  }
+}
+
+class PartTimeEmployee extends Employee {
+  getPay(): number {
+    return 6.5;
+  }
+}
